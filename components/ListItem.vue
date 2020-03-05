b3ceb08f6d94e8b972d40f6a50fc285e544658a1
Muhammad Abdul Karim <alkariim.tridek@gmail.com>
2020-03-05 17:06:01 +0700

    Bismillah

---
+++ components/ListItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <tr>
+    <td>{{ data.id }}</td>
+    <td>{{ data.title }}</td>
+    <td>{{ data.completed ? "Selesai" : "Belum Selesai" }}</td>
+    <td>View, Edit, Delete</td>
+  </tr>
+</template>
+
+<script>
+export default {
+  props: ["data"]
+};
+</script>
+<style scoped></style>
---
+++ pages/todo/index.vue
@@ -0,0 +1,84 @@
+<template>
+  <b-container fluid="md">
+    <div v-if="isLoading">
+      <b-spinner type="grow" label="Loading..."></b-spinner>
+    </div>
+    <div v-else-if="isError">
+      <b-alert show variant="danger">Error Bro</b-alert>
+    </div>
+    <div v-else-if="isEmpty">
+      <b-alert show variant="info">Data Kosong</b-alert>
+    </div>
+    <div v-else>
+      <h1>Todo List</h1>
+      <table border="1 solid">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Status</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <TodoListItem :data="todo" v-for="todo in todos" :key="todo.id" />
+        </tbody>
+      </table>
+    </div>
+  </b-container>
+</template>
+
+<script>
+import TodoListItem from "~/components/ListItem";
+import Axios from "axios";
+
+export default {
+  components: { TodoListItem },
+  data() {
+    return {
+      todos: [],
+      isLoading: false,
+      isEmpty: false,
+      isError: false
+    };
+  },
+  mounted() {
+    this.isLoading = true;
+
+    Axios.get("https://jsonplaceholder.typicode.com/todos")
+      .then(res => {
+        this.todos = res.data;
+
+        if (this.todos.length === 0) {
+          this.isEmpty = true;
+        }
+
+        this.isLoading = false;
+      })
+      .catch(err => {
+        console.error(err);
+
+        this.isError = true;
+        this.isLoading = false;
+      });
+  }
+  // async mounted() {
+  //   this.isLoading = true;
+  //   try {
+  //     const res = await Axios.get("https://jsonplaceholder.typicode.com/todos");
+
+  //     this.todos = res.data;
+
+  //     if (this.todos.length === 0) {
+  //       this.isEmpty = true;
+  //     }
+  //   } catch (error) {
+  //     console.error(error);
+
+  //     this.isError = true;
+  //   }
+  //   this.isLoading = false;
+  // }
+};
+</script>
+<style scoped></style>
